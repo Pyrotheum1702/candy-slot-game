b6890a5469e43585c386af89513249f11fecd97c
PyroMacMini <pyrotheum1702@gmail.com>
2025-08-05 02:12:01 +0700

    add base ui interact handling

--- assets/Script/MainScene/SlotGame/SlotGameUIController.ts
+++ assets/Script/MainScene/SlotGame/SlotGameUIController.ts
@@ -2,5 +2,43 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class SlotGameUIController extends cc.Component {
+   public onClickSpin() {
 
+   }
+
+   public onClickHistory() {
+
+   }
+
+   public onClickAutoSpin() {
+
+   }
+
+   public onClickAdjustSpeed() {
+
+   }
+
+   public onClickChangeLineCount() {
+
+   }
+
+   public onClickBuyBonus() {
+
+   }
+
+   public onClickIncrementBet() {
+
+   }
+
+   public onClickDecrementBet() {
+
+   }
+
+   public onClickMinBet() {
+
+   }
+
+   public onClickMaxBet() {
+
+   }
 }
