2244b82870ef5dd3d00d3f9735ea18feaa7faf0b
PyroMacMini <pyrotheum1702@gmail.com>
2025-08-05 00:15:05 +0700

    setup SpinResultPanel elements layout

--- assets/Script/Config/GameConfig.ts
+++ assets/Script/Config/GameConfig.ts
@@ -2,6 +2,10 @@ export const GAME_CONFIG = {
    gameName: `SlotCandy`,
    newPlayerInitBalance: 1000,
    playerBalanceCap: 1_000_000_000,
+   slotGame: {
+      gridRow: 3,
+      gridColumn: 3,
+   }
 }
 
 // author: `Pyro`
